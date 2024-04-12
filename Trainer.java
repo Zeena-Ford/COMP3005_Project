452f983bb6ff00699e7f28d236ce7af5f77279a0
Zeena-Ford <121990470+Zeena-Ford@users.noreply.github.com>
2024-04-12 05:20:52 -0400

    Update Trainer.java

--- Trainer.java
+++ Trainer.java
@@ -1,113 +1,134 @@
+/**
+ *  Trainer class representing the trainers in a Gym
+ *
+ * @author Zeena Ford 101229954
+ * @version April 12, 2024
+ */
+
+
 import java.sql.*;
 
 public class Trainer {
     AdministrativeClass ad = new AdministrativeClass();
     Member mem1 = new Member();
-    public void scheduleManagement(String trainerName, String timeFrame) { //right order in parameter
 
+    /**
+     * Allows trainers to set the time for which they are available and schedule a fitness class
+     * @param trainerName represents the first name of the trainer
+     * @param timeFrame represents the time that the trainer is available
+     */
+    public void scheduleManagement(String trainerName, String timeFrame) { //right order in parameter
+        //url code containing the local host, user, password and name for the database
         String url = "jdbc:postgresql://localhost:5432/health and fitness management system";
         String user = "postgres";
         String password = "datapass";
         try {
             Class.forName("org.postgresql.Driver");
-            Connection connection = DriverManager.getConnection(url, user, password);
-            Statement statement = connection.createStatement();
-            String sqls = "UPDATE schedule SET workout_time = ? WHERE trainer_name = ?; UPDATE rooms SET workout_time = ? WHERE trainer_name = ?; UPDATE rooms SET maintenance = 'being used' WHERE workout_time = ? "; //INSERT INTO schedule (trainer_name, member_name, available_date, workout_time, room_number) VALUES (?, ?, ?, ?, ?)";
+            //enabling connection between the database and the application using the provided driver
+            Connection connectSQL = DriverManager.getConnection(url, user, password);
+            Statement statement = connectSQL.createStatement();
+            //executing database query to output the records from 'students' database
+            String querySQL = "UPDATE schedule SET workout_time = ? WHERE trainer_name = ?; UPDATE rooms SET workout_time = ? WHERE trainer_name = ?; UPDATE rooms SET maintenance = 'being used' WHERE workout_time = ? "; //INSERT INTO schedule (trainer_name, member_name, available_date, workout_time, room_number) VALUES (?, ?, ?, ?, ?)";
 
             //ResultSet resultSet = statement.getResultSet();
-            PreparedStatement preparedStatement = connection.prepareStatement(sqls);
-            preparedStatement.setString(1, timeFrame);
-            preparedStatement.setString(2, trainerName);
-            preparedStatement.setString(3, timeFrame);
-            preparedStatement.setString(4, trainerName);
-            preparedStatement.setString(5, timeFrame);
-            //statement.executeUpdate(sqls);
-            preparedStatement.executeUpdate();
-            statement.executeQuery("select * from SCHEDULE");
+            PreparedStatement prepState = connectSQL.prepareStatement(querySQL);
+            prepState.setString(1, timeFrame);
+            prepState.setString(2, trainerName);
+            prepState.setString(3, timeFrame);
+            prepState.setString(4, trainerName);
+            prepState.setString(5, timeFrame);
+            //statement.executeUpdate(querySQL);
+            prepState.executeQuery();
+            //statement.executeQuery("select * from SCHEDULE");
+            //calling roomBookingManagement function to allow the trainer to book a workout room once they have scheduled a workout class into the SCHEDULE table with the timeframe included
             ad.roomBookingManagement(timeFrame);
 
-            //statement.executeUpdate(sqls);
+            //statement.executeUpdate(querySQL);
             //statement.executeQuery("select * from STUDENTS");
-            ResultSet resultSet = statement.getResultSet();
+            ResultSet resultSQL = prepState.getResultSet();
             //iterate through the table to output the provided results to display the records including the updated email
-            //while (resultSet.next()) {
-                //System.out.println(resultSet.getString("trainer_name"));
-                //System.out.println(resultSet.getString("member_name"));
-               // System.out.println(resultSet.getString("available_date"));
-               // System.out.println(resultSet.getString("workout_time"));
-               // System.out.println(resultSet.getString("room_number"));
-           // }
-
+            while (resultSQL.next()) { //fourth difference
+                System.out.println("trainer_name     member_name        available_date          workout_time       room_number" + "\n");
+                System.out.println(resultSQL.getString("trainer_name") + "\t\t\t" + resultSQL.getString("member_name") + "\t\t\t" + resultSQL.getString("available_date") + "\t\t\t" + resultSQL.getString("workout_time")+ "\t\t\t" + resultSQL.getString("room_number"));
+            }
+            //catch exception (SQL Exception included) and print results
         } catch (Exception e) {
             System.out.println(e);
         }
     }
 
+    /**
+     * Allows trainers to view the profile of the member they search
+     * @param firstName represents the first name of the member that the trainer searches
+     * @param lastName represents the last name of the member that the trainer searches
+     */
+
     public void memberProfileViewing(String firstName, String lastName){
+        //url code containing the local host, user, password and name for the database
         String url = "jdbc:postgresql://localhost:5432/health and fitness management system";
         String user = "postgres";
         String password = "datapass";
         try {
             Class.forName("org.postgresql.Driver");
-            Connection connection = DriverManager.getConnection(url, user, password);
-            Statement statement = connection.createStatement();
+            //enabling connection between the database and the application using the provided driver
+            Connection connectSQL = DriverManager.getConnection(url, user, password);
+            Statement statement = connectSQL.createStatement();
+            //executing database query to allow the trainer to display the view of a member's profile
 
-            String sqls = "SELECT first_name, last_name, email, phone, join_date, credit_card_balance FROM members WHERE first_name = ? AND last_name = ?"; // WHERE workout_time = ? //UPDATE schedule SET member_name = ? WHERE workout_time = ? AND member_name IS NULL";
-            PreparedStatement preparedStatement = connection.prepareStatement(sqls);
-            preparedStatement.setString(1, firstName);
-            preparedStatement.setString(2, lastName);
-            preparedStatement.executeUpdate();
-            statement.executeQuery("select * from MEMBERS");
+            String querySQL = "SELECT first_name, last_name, email, phone, join_date, credit_card_balance FROM members WHERE first_name = ? AND last_name = ?"; // WHERE workout_time = ? //UPDATE schedule SET member_name = ? WHERE workout_time = ? AND member_name IS NULL";
+            PreparedStatement prepState = connectSQL.prepareStatement(querySQL);
+            prepState.setString(1, firstName);
+            prepState.setString(2, lastName);
+            prepState.executeQuery();
 
-
-
-            ResultSet resultSet = preparedStatement.getResultSet();
-            System.out.println(resultSet.getString("first_name"));
-            System.out.println(resultSet.getString("last_name"));
-            System.out.println(resultSet.getString("email"));
-            System.out.println(resultSet.getString("phone"));
-            System.out.println(resultSet.getString("join_date"));
-            System.out.println(resultSet.getString("credit_card_balance"));
+            ResultSet resultSQL = prepState.getResultSet();
             //iterate through the table to output the provided results to display the records including the updated email
-            //while (resultSet.next()) {
-            //System.out.println(resultSet.getString("first_name"));
-            // System.out.println(resultSet.getString("last_name"));
-            // System.out.println(resultSet.getString("goals"));
-            // System.out.println(resultSet.getString("achievements"));
-            //System.out.println(resultSet.getString("active_time"));
-            //}
-
+            while (resultSQL.next()) {
+                System.out.println("first_name       last_name                email                     phone               join_date      credit_card_balance" + "\n");
+                System.out.println(resultSQL.getString("first_name") + "\t\t\t\t\t" + resultSQL.getString("last_name") + "\t\t\t" + resultSQL.getString("email") + "\t\t\t" + resultSQL.getString("phone")+ "\t\t\t" + resultSQL.getString("join_date")+ "\t\t\t" + resultSQL.getString("credit_card_balance"));
+            }
+            //catch exception (SQL Exception included) and print results
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
         }
     }
 
+    /**
+     * Allows trainers to end their workout class
+     * @param trainerName represents the first name of the trainer
+     * @param timeFrame represents the time that the workout began
+     * @param clientMember represents the client (member) that the trainer is training
+     */
+
     public void endWorkoutClass(String trainerName, String timeFrame, String clientMember){
+        //url code containing the local host, user, password and name for the database
         String url = "jdbc:postgresql://localhost:5432/health and fitness management system";
         String user = "postgres";
         String password = "datapass";
         try {
             Class.forName("org.postgresql.Driver");
-            Connection connection = DriverManager.getConnection(url, user, password);
-            Statement statement = connection.createStatement();
+            //enabling connection between the database and the application using the provided driver
+            Connection connectSQL = DriverManager.getConnection(url, user, password);
+            Statement statement = connectSQL.createStatement();
             mem1.appleWatch(clientMember);
+            //calling equipmentMaintenanceMonitoring to allow the maintenance staff to clean the workout room once the trainer has ended the class
             ad.equipmentMaintenanceMonitoring(trainerName, timeFrame);
-            String sqls = "UPDATE schedule SET workout_time = NULL WHERE trainer_name = ?; UPDATE rooms SET workout_time = NULL WHERE trainer_name = ?; UPDATE rooms SET status = 'room pending' WHERE trainer_name = ?";
+            String querySQL = "UPDATE schedule SET workout_time = NULL WHERE trainer_name = ?; UPDATE rooms SET workout_time = NULL WHERE trainer_name = ?; UPDATE rooms SET status = 'room pending' WHERE trainer_name = ?";
 
             //ResultSet resultSet = statement.getResultSet();
-            PreparedStatement preparedStatement = connection.prepareStatement(sqls);
-            preparedStatement.setString(1, trainerName);
-            preparedStatement.setString(2, trainerName);
-            preparedStatement.setString(3, trainerName);
+            PreparedStatement prepState = connectSQL.prepareStatement(querySQL);
+            prepState.setString(1, trainerName);
+            prepState.setString(2, trainerName);
+            prepState.setString(3, trainerName);
 
-            //statement.executeUpdate(sqls);
-            preparedStatement.executeUpdate();
+            //statement.executeUpdate(querySQL);
+            prepState.executeUpdate();
             statement.executeQuery("select * from SCHEDULE");
-            //statement.executeUpdate(sqls);
+            //statement.executeUpdate(querySQL);
             //statement.executeQuery("select * from STUDENTS");
             ResultSet resultSet = statement.getResultSet();
 
-
+            //catch exception (SQL Exception included) and print results
 
         } catch (Exception e) {
             System.out.println(e);
@@ -121,3 +142,9 @@ public class Trainer {
 
 
 }
+
+
+
+
+
+
