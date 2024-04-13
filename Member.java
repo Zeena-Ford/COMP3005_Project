b67c545c74b5e8c1e82abd6e77785a429b3dfba3
Zeena-Ford <121990470+Zeena-Ford@users.noreply.github.com>
2024-04-13 09:33:30 -0400

    Update Member.java

--- Member.java
+++ Member.java
@@ -1,7 +1,7 @@
 /**
  *  Member class representing the members in a Gym
  *
- * @author Zeena Ford 101229954
+ * @author Zeena Ford 101229954, Zeina Mouhtadi 101169685
  * @version April 12, 2024
  */
 
