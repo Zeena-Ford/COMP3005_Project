4a7da7565b2a6e0b0951e833386b175dd8897e1c
Zeena-Ford <121990470+Zeena-Ford@users.noreply.github.com>
2024-04-14 00:16:58 -0400

    Update Gym.java

--- Gym.java
+++ Gym.java
@@ -5,7 +5,9 @@
  * @version April 12, 2024
  */
 
+
 import java.sql.*;
+
 
 /**
  * Main function to test each function (and bonus functions)
@@ -14,18 +16,28 @@ public class Gym {
 
     public static void main(String[] args) {
 
-        //Member m1 = new Member();
-        //Trainer t1 = new Trainer();
-        //AdministrativeClass adm1 = new AdministrativeClass();
+        Member m1 = new Member();
+        Trainer t1 = new Trainer();
+        AdministrativeStaff adm1 = new AdministrativeStaff();
 
 
-        //t1.scheduleManagement("Emily", "5pm");
+
+        //m1.userRegistration("mil", "doe", "mildoe@gmail.com", "613-218-2248", Date.valueOf("2023-04-04"), 2000);
+        //m1.profileManagement("Lilly", "Blake", "goals", "deadlift 200 pounds");
+        //m1.dashboardDisplay("Krist", "Loth");
+        //t1.scheduleManagement("Kevin", "5pm", "booked");
         //m1.scheduleManagement("Krist", "Loth", "5pm");
-        //t1.endWorkoutClass("Emily", "5pm", "Krist");
-        //m1.userRegistration("Lea", "Mol", "leamol@gmail.com", "613-212-2222", Date.valueOf("2023-04-04"), 2000);
-        //m1.profileManagement("Ema", "Smith", "goals", "Abs");
-        //m1.dashboardDisplay("milly", "Doe");
-        //t1.memberProfileViewing("Bob", "Jona");
+        //t1.scheduleManagement("Dan", "4am", "booked");
+        //t1.scheduleManagement("Dan", "4am", "canceled");
+        //t1.scheduleManagement("Dawson", "7am", "booked");
+        //t1.memberProfileViewing("Ben", "Flore");
+        //t1.endWorkoutClass("Kevin", "5pm", "Krist");
+        //adm1.classScheduleUpdating("Dawson", "7am", "canceled");
+        //adm1.billingAndPaymentHistory("Ben", "Flore", "weights", 55);
+        //adm1.roomBookingManagement("10pm", "Kevin", "canceled");
+        //adm1.resetDay();
+
+
 
     }
 
